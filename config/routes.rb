b5e65e684e56396484112894b5b53b5cfa8e9d4d
hiroyuki yamaki <uu137582@RLSUU137582M01.local>
2018-06-07 17:59:25 +0900

    7.5: Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,16 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'
+  get '/signup', to: 'users#new'
 
+  ### resourcesでrouteを定義するとRESTful URLセットが使えるようになる
+  # HTTPリクエスト	URL	アクション	名前付きルート	用途
+  # GET	/users	index	users_path	すべてのユーザーを一覧するページ
+  # GET	/users/1	show	user_path(user)	特定のユーザーを表示するページ
+  # GET	/users/new	new	new_user_path	ユーザーを新規作成するページ (ユーザー登録)
+  # POST	/users	create	users_path	ユーザーを作成するアクション
+  # GET	/users/1/edit	edit	edit_user_path(user)	id=1のユーザーを編集するページ
+  # PATCH	/users/1	update	user_path(user)	ユーザーを更新するアクション
+  # DELETE	/users/1	destroy	user_path(user)	ユーザーを削除するアクション
+  resources :users
 end
